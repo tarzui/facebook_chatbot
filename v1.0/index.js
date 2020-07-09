9f2ce9af4032c60c3f88c0fcfc63b983210534a4
kiitar <chakri.psu@gmail.com>
2020-07-09 14:56:16 +0700

    up

--- v1.0/index.js
+++ v1.0/index.js
@@ -1,16 +1,64 @@
 const express = require("express");
 const Config = require("../config");
 const request = require("request");
+const axios = require("axios");
 
 const router = express.Router();
+const token =
+  "EAAD8t3nWfNUBAJxkCAJO0cM97wS6if67ZCIQFj9NvSLWGfuBwtHIEOEVM6wHMEEnGVP6vVXmsSzXuXs8NwlvtNeUxwmjADMIuCjre5p3orim6mPz4Ax3ZBZCrlp5fveJ4ZCFNRKsFIEdUZBZBYkjFZB1GPhNS8ke57oFzX5DrZADS8hMOTDlJkPxcyslFv8GuI4ZD";
 
 router.get("/", (req, res) => {
   res.send("Hello facebook bot");
 });
 
-router.post("/webhook", (req, res) => {
-  console.log("webhook");
+// router.get("/webhook", (req, res) => {
+//   console.log("rq :", req);
+//   // Your verify token. Should be a random string.
+//   let VERIFY_TOKEN = "facebookbotbtf";
 
+//   // Parse the query params
+//   let mode = req.query["hub.mode"];
+//   let token = req.query["hub.verify_token"];
+//   let challenge = req.query["hub.challenge"];
+
+//   // Checks if a token and mode is in the query string of the request
+//   if (mode && token) {
+//     // Checks the mode and token sent is correct
+//     if (mode === "subscribe" && token === VERIFY_TOKEN) {
+//       // Responds with the challenge token from the request
+//       console.log("WEBHOOK_VERIFIED");
+//       res.status(200).send(challenge);
+//     } else {
+//       // Responds with '403 Forbidden' if verify tokens do not match
+//       res.sendStatus(403);
+//     }
+//   }
+// });
+
+function sendTextMessage(sender, text) {
+  let messageData = { text: text };
+
+  request(
+    {
+      url: "https://graph.facebook.com/v2.6/me/messages",
+      qs: { access_token: token },
+      method: "POST",
+      json: {
+        recipient: { id: sender },
+        message: messageData,
+      },
+    }
+    // function (error, response, body) {
+    //   if (error) {
+    //     console.log("Error sending messages: ", error);
+    //   } else if (response.body.error) {
+    //     console.log("Error: ", response.body.error);
+    //   }
+    // }
+  );
+}
+
+router.post("/webhook", async (req, res) => {
   let messaging_events = req.body.entry[0].messaging;
   for (let i = 0; i < messaging_events.length; i++) {
     let event = req.body.entry[0].messaging[i];
@@ -19,58 +67,44 @@ router.post("/webhook", (req, res) => {
       let text = event.message.text;
       if (text === "Generic") {
         console.log("welcome to chatbot");
-        //sendGenericMessage(sender)
         continue;
       }
-      sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200));
+
+      let data = JSON.stringify({
+        query:
+          "query message($message:String!){\n    message(message:$message){\n    data{\n      found\n      intent\n      confidence\n    }\n  }\n}",
+        variables: { message: `${text}` },
+      });
+
+      let config = {
+        method: "post",
+        url: "http://103.245.164.59:3004/api_management",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        data: data,
+      };
+
+      axios(config)
+        .then(function (response) {
+          // console.log(JSON.stringify(response.data.data.message.data.intent));
+          let rep = response.data.data.message.data.intent;
+          console.log(sender, text);
+          sendTextMessage(sender, `${rep}`);
+        })
+        .catch(function (error) {
+          console.log("tar : ", error);
+        });
     }
-    if (event.postback) {
-      let text = JSON.stringify(event.postback);
-      console.log("tar : ", text);
-      // sendTextMessage(sender, "Postback received: " + text.substring(0, 200), token);
-      continue;
-    }
+    // if (event.postback) {
+    //   let text = JSON.stringify(event.postback);
+    //   console.log("postback");
+    //   // sendTextMessage(sender, "Postback received: " + text.substring(0, 200), token);
+    //   continue;
+    // }
   }
 
-  let body = req.body;
-
-  // Checks this is an event from a page subscription
-  if (body.object === "page") {
-    // Iterates over each entry - there may be multiple if batched
-    body.entry.forEach(function (entry) {
-      // Gets the message. entry.messaging is an array, but
-      // will only ever contain one message, so we get index 0
-      let webhook_event = entry.messaging[0];
-      console.log(webhook_event);
-    });
-
-    // Returns a '200 OK' response to all requests
-    res.status(200).send("EVENT_RECEIVED");
-  } else {
-    // Returns a '404 Not Found' if event is not from a page subscription
-    res.sendStatus(404);
-  }
-
-  // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = "btf";
-
-  // Parse the query params
-  let mode = req.query["hub.mode"];
-  let token = req.query["hub.verify_token"];
-  let challenge = req.query["hub.challenge"];
-
-  // Checks if a token and mode is in the query string of the request
-  if (mode && token) {
-    // Checks the mode and token sent is correct
-    if (mode === "subscribe" && token === VERIFY_TOKEN) {
-      // Responds with the challenge token from the request
-      console.log("WEBHOOK_VERIFIED");
-      res.status(200).send(challenge);
-    } else {
-      // Responds with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403);
-    }
-  }
+  res.sendStatus(200);
 });
 
 module.exports = router;
